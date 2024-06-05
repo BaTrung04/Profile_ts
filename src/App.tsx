3cee9d4d342f1b37d1befffc36da1b32cbc61503
BaTrung04 <trungmkzxc12345@gmail.com>
2024-06-05 23:24:52 +0700

    chia component

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,7 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
-import Video9 from "./examples/Video9";
-import Video10 from "./examples/Video10/Video10";
-import Video13 from "./examples/Video13";
+import Portfolio from "./screens/portfilio";
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        {/* <Video5 name="trung" /> */}
-        {/* <Video13 //name={"trung2"}
-        /> */}
-        <Video10 defaultAge={22} defaultName={"trung"} />
-      </p>
-    </div>
-  );
+  return <Portfolio />;
 }
 
 export default App;
--- src/examples/Video7.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-const Video7: React.FC = () => {
-  const name = "trung";
-  return <div>Example in video 7: hello world 333366</div>;
-};
-
-export default Video7;
