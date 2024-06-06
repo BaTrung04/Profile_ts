d7b1e96cb78668ccaaa8c339d9aaa83fac10525d
BaTrung04 <trungmkzxc12345@gmail.com>
2024-06-06 16:33:47 +0700

    skill

--- src/components/content/skill.tsx
+++ src/components/content/skill.tsx
@@ -1,5 +1,202 @@
+import ProgressBar from "@ramonak/react-progress-bar";
+
 const Skill = () => {
-  return <></>;
+  return (
+    <div className="arlo_tm_section" id="skills">
+      <div className="arlo_tm_skills_wrap">
+        <div className="container">
+          <div className="inner_wrap">
+            <div className="leftbox">
+              <div className="arlo_tm_mini_title_holder">
+                <h4>Kỹ năng Frontend</h4>
+              </div>
+              <li>Cắt ghép </li>
+              <li>Html </li>
+            </div>
+            <div className="rightbox">
+              <div className="progress_bar_wrap_total">
+                <div
+                  className="arlo_tm_progress_wrap"
+                  data-size="small"
+                  data-round="c"
+                  data-strip="off"
+                >
+                  <div
+                    className="arlo_tm_progress"
+                    data-value="95"
+                    data-color="#000"
+                  >
+                    <span>
+                      <span className="label">HTML/CSS/JavaScript</span>
+                      <span className="number">95%</span>
+                    </span>
+                    <div className="arlo_tm_bar_bg">
+                      <ProgressBar
+                        completed={95}
+                        bgColor={"#333"}
+                        height={"8px"}
+                        isLabelVisible={false}
+                      />
+                    </div>
+                  </div>
+                  <div
+                    className="arlo_tm_progress"
+                    data-value="95"
+                    data-color="#000"
+                  >
+                    <span>
+                      <span className="label">HTML/CSS/JavaScript</span>
+                      <span className="number">95%</span>
+                    </span>
+                    <div className="arlo_tm_bar_bg">
+                      <ProgressBar
+                        completed={95}
+                        bgColor={"#333"}
+                        height={"8px"}
+                        isLabelVisible={false}
+                      />
+                    </div>
+                  </div>
+                  <div
+                    className="arlo_tm_progress"
+                    data-value="95"
+                    data-color="#000"
+                  >
+                    <span>
+                      <span className="label">HTML/CSS/JavaScript</span>
+                      <span className="number">95%</span>
+                    </span>
+                    <div className="arlo_tm_bar_bg">
+                      <ProgressBar
+                        completed={95}
+                        bgColor={"#333"}
+                        height={"8px"}
+                        isLabelVisible={false}
+                      />
+                    </div>
+                  </div>
+                  <div
+                    className="arlo_tm_progress"
+                    data-value="95"
+                    data-color="#000"
+                  >
+                    <span>
+                      <span className="label">HTML/CSS/JavaScript</span>
+                      <span className="number">95%</span>
+                    </span>
+                    <div className="arlo_tm_bar_bg">
+                      <ProgressBar
+                        completed={95}
+                        bgColor={"#333"}
+                        height={"8px"}
+                        isLabelVisible={false}
+                      />
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="arlo_tm_skills_wrap">
+        <div className="container">
+          <div className="inner_wrap">
+            <div className="leftbox">
+              <div className="arlo_tm_mini_title_holder">
+                <h4>Kỹ năng Frontend</h4>
+              </div>
+              <li>Cắt ghép </li>
+              <li>Html </li>
+            </div>
+            <div className="rightbox">
+              <div className="progress_bar_wrap_total">
+                <div
+                  className="arlo_tm_progress_wrap"
+                  data-size="small"
+                  data-round="c"
+                  data-strip="off"
+                >
+                  <div
+                    className="arlo_tm_progress"
+                    data-value="95"
+                    data-color="#000"
+                  >
+                    <span>
+                      <span className="label">HTML/CSS/JavaScript</span>
+                      <span className="number">95%</span>
+                    </span>
+                    <div className="arlo_tm_bar_bg">
+                      <ProgressBar
+                        completed={95}
+                        bgColor={"#333"}
+                        height={"8px"}
+                        isLabelVisible={false}
+                      />
+                    </div>
+                  </div>
+                  <div
+                    className="arlo_tm_progress"
+                    data-value="95"
+                    data-color="#000"
+                  >
+                    <span>
+                      <span className="label">HTML/CSS/JavaScript</span>
+                      <span className="number">95%</span>
+                    </span>
+                    <div className="arlo_tm_bar_bg">
+                      <ProgressBar
+                        completed={95}
+                        bgColor={"#333"}
+                        height={"8px"}
+                        isLabelVisible={false}
+                      />
+                    </div>
+                  </div>
+                  <div
+                    className="arlo_tm_progress"
+                    data-value="95"
+                    data-color="#000"
+                  >
+                    <span>
+                      <span className="label">HTML/CSS/JavaScript</span>
+                      <span className="number">95%</span>
+                    </span>
+                    <div className="arlo_tm_bar_bg">
+                      <ProgressBar
+                        completed={95}
+                        bgColor={"#333"}
+                        height={"8px"}
+                        isLabelVisible={false}
+                      />
+                    </div>
+                  </div>
+                  <div
+                    className="arlo_tm_progress"
+                    data-value="95"
+                    data-color="#000"
+                  >
+                    <span>
+                      <span className="label">HTML/CSS/JavaScript</span>
+                      <span className="number">95%</span>
+                    </span>
+                    <div className="arlo_tm_bar_bg">
+                      <ProgressBar
+                        completed={95}
+                        bgColor={"#333"}
+                        height={"8px"}
+                        isLabelVisible={false}
+                      />
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Skill;
--- src/components/leftpart.tsx
+++ src/components/leftpart.tsx
@@ -17,10 +17,10 @@ const LeftPart = () => {
                 <a href="#home">Home</a>
               </li>
               <li>
-                <a href="#about">About</a>
+                <a href="#about">About me</a>
               </li>
               <li>
-                <a href="#portfolio">Portfolio</a>
+                <a href="#skills">Skills</a>
               </li>
               <li>
                 <a href="#news">News</a>
